2611502b4c487b11d0ab40609f72579a8d116c62
Yashashya <yashashya@gmail.com>
2024-05-10 19:04:02 +0530

    10/05/2024
    commit

--- src/modules/users/CreateUser.js
+++ src/modules/users/CreateUser.js
@@ -1,59 +1,58 @@
 import React, { useEffect, useState } from 'react'
 
+import axios from 'axios';
 export const CreateUser = () => {
 
-    //var email = "abc@123";
+  const [form, setForm] = useState({
 
-    const [email,setEmail] = useState("abc@123.com");
-    const [numb,setNumb] = useState([]);
+    name:"",
+    address:"",
+    mobile:"",
+    email:"",
+    id:""
 
-    const changeEmail = (event)=>{
+  })
 
-       
-       setEmail(event.target.value);
-    // email = event.target.value;
-
-    }
-
-    const counting = ()=>{
-
-        for(let i=0;i<9;i++)
-        {
-            setNumb([...numb,{test:"TESTING "+i}])
-
-            console.log(i);
-        }
-      
-    }
-   
-
-    useEffect(() => {
-      
-        counting();
-      var countInterval =   setInterval(()=>{counting()},5000);
-         
-      return () => {
-
-        clearInterval(countInterval);
-
-       alert("hello");
-      }
-    }, [])
     
+
+  const changeHandler = (event) =>{
+
+
+      setForm({...form,[event.target.name]:event.target.value});
+
+
+  }
+
+
+  const submithandler = async (event) =>{
+
+   let  checkerror = false;
+
+   try{
+
+        let response = await axios.post("http://localhost/user-create",form);
+
+
+        console.log("for response");
+        console.log("for response",response);
+      
+   }catch(e){
+
+      alert("network lost "+e);
+   }
+ 
+
+  }
+
+    
+
+   
  
   
 
   return (
     <>
 
-   {
-
-        numb.map((row,i)=>{
-            return <li key={i}>{row.test}</li>
-        })
-
-
-   }
        <div className="content-wrapper" style={{ minHeight: "1302.12px" }}>
   {/* Content Header (Page header) */}
   <div className="content-header">
@@ -68,63 +67,40 @@ export const CreateUser = () => {
       </div>
       {/* /.card-header */}
       {/* form start */}
-      <form>
+      <div>
         <div className="card-body">
           <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input
-              type="email"
-              defaultValue={email}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter email"
-              onChange={changeEmail}
-            />
+            <label>Name</label> 
+            <input type="text" className="form-control" name='name' defaultValue={form.name} onChange={changeHandler} />
           </div>
           <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Confirm Email</label>
-            <input
-              type="text"
-              className="form-control"
-                value={email}
-            />
+            <label>address</label>
+            <input type="text" className="form-control" name='address' defaultValue={form.address} onChange={changeHandler} />
           </div>
           <div className="form-group">
-            <label htmlFor="exampleInputFile">File input</label>
-            <div className="input-group">
-              <div className="custom-file">
-                <input
-                  type="file"
-                  className="custom-file-input"
-                  id="exampleInputFile"
-                />
-                <label className="custom-file-label" htmlFor="exampleInputFile">
-                  Choose file
-                </label>
-              </div>
-              <div className="input-group-append">
-                <span className="input-group-text">Upload</span>
-              </div>
-            </div>
+            <label>mobile no</label>
+            <input type="text" className="form-control" name='mobile' defaultValue={form.mobile} onChange={changeHandler} />
           </div>
-          <div className="form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="exampleCheck1"
-            />
-            <label className="form-check-label" htmlFor="exampleCheck1">
-              Check me out
-            </label>
+          <div className="form-group">
+            <label>id card</label>
+            <input type="text" className="form-control" name='id' defaultValue={form.id} onChange={changeHandler} />
           </div>
+          <div className="form-group">
+            <label>email</label>
+            <input type="text" className="form-control" name='email' defaultValue={form.email} onChange={changeHandler} />
+          </div>
+          
+
+          
+       
         </div>
         {/* /.card-body */}
         <div className="card-footer">
-          <button type="submit" className="btn btn-primary">
+          <button type="button" className="btn btn-primary" onClick={submithandler}>
             Submit
           </button>
         </div>
-      </form>
+      </div>
     </div>
    </div>
 </div>
